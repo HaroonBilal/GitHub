99ee791e96e1d22758489f133f60bee8342f8477
HaroonBilal <haroon.bilal@protonmail.com>
2020-04-15 00:34:34 +0100

    working

--- routes/authors.js
+++ routes/authors.js
@@ -1,20 +1,54 @@
-const express = require("express");
-const router = express.Router();
-const Author = require("../models/authors");
+const express = require("express")
+const router = express.Router()
+const Author = require("../models/author")
 
 // All Authors Route
 
 router.get("/", (req, res) => {
-  res.render("authors/index");
+  res.render("authors/index")
 });
 
 // New Authors routes
 router.get("/new", (req, res) => {
-  res.render("authors/new", { author: new Author() });
-});
-//Create Authors
-router.post("/", (req, res) => {
-  res.send(req.body.name);
+  res.render("authors/new", { author: new Author() })
 });
 
-module.exports = router;
+
+//Create Authors Route
+
+router.post("/", async (req, res) => {
+  const author = new Author({
+    name: req.body.name
+  })
+  try {
+    const newAuthor = await author.save()
+    res.redirect('authors')
+
+  } catch (err) {
+    res.render('authors/new', {
+      author: author,
+      errorMessage: 'Error creating Author',
+      checkMessage: 'hey you go there'
+
+
+    })
+  }
+  // router.post("/", (req, res) => {
+  //   const author = new Author({
+  //     name: req.body.name
+  //   })
+  //   author.save((err, newAuthor) => {
+  //     if (err) {
+  //       res.render('authors/new', {
+  //         author: author,
+  //         errorMessage: 'Error creating Author'
+
+  //       })
+  //     } else {
+  //       res.redirect('authors')
+  //     }
+  //   })
+  //   //res.send(req.body.name)
+})
+
+module.exports = router
