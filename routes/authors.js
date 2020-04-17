18b966e8c46483997bd58b2a5516cfa2888b5bd6
HaroonBilal <haroon.bilal@protonmail.com>
2020-04-17 22:24:17 +0100

    working

--- routes/authors.js
+++ routes/authors.js
@@ -1,54 +1,47 @@
-const express = require("express")
-const router = express.Router()
-const Author = require("../models/author")
+const express = require("express");
+const router = express.Router();
+const Author = require("../models/author");
 
 // All Authors Route
 
-router.get("/", (req, res) => {
-  res.render("authors/index")
+router.get("/", async (req, res) => {
+  let searchOptions = {};
+  if (req.query.name != null && req.query.name != "") {
+    searchOptions.name = new RegExp(req.query.name, "i");
+  }
+  try {
+    const authors = await Author.find(searchOptions);
+    res.render("authors/index", {
+      authors: authors,
+      searchOptions: req.query,
+    });
+  } catch (err) {
+    res.redirect("/");
+  }
+  res.render("authors/index");
 });
 
 // New Authors routes
 router.get("/new", (req, res) => {
-  res.render("authors/new", { author: new Author() })
+  res.render("authors/new", { author: new Author() });
 });
-
 
 //Create Authors Route
 
 router.post("/", async (req, res) => {
   const author = new Author({
-    name: req.body.name
-  })
+    name: req.body.name,
+  });
   try {
-    const newAuthor = await author.save()
-    res.redirect('authors')
-
+    const newAuthor = await author.save();
+    //
+    res.redirect("/authors");
   } catch (err) {
-    res.render('authors/new', {
+    res.render("authors/new", {
       author: author,
-      errorMessage: 'Error creating Author',
-      checkMessage: 'hey you go there'
-
-
-    })
+      errorMessage: "Error creating Author",
+    });
   }
-  // router.post("/", (req, res) => {
-  //   const author = new Author({
-  //     name: req.body.name
-  //   })
-  //   author.save((err, newAuthor) => {
-  //     if (err) {
-  //       res.render('authors/new', {
-  //         author: author,
-  //         errorMessage: 'Error creating Author'
+});
 
-  //       })
-  //     } else {
-  //       res.redirect('authors')
-  //     }
-  //   })
-  //   //res.send(req.body.name)
-})
-
-module.exports = router
+module.exports = router;
