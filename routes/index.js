cab23a02b25f241eaaaf7f851a037c58bc56c22b
HaroonBilal <haroon.bilal@protonmail.com>
2020-04-20 23:21:39 +0100

    css

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,3 @@
-// const express  = require('express')
-// const router = express.Router()
-
-// router.get('/', (req, res) => {
-//     res.render('index')
-
-// })
-
-// module.exports = router
 
 const express = require('express')
 const router = express.Router()
